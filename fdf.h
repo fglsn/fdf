e5b78a81cd7347a2f8588d5572ab781fb246edf9
Ilona Shakurova <leverseau19@gmail.com>
2022-03-14 11:46:50 +0200

    Proceed with reading

--- fdf.h
+++ fdf.h
@@ -6,18 +6,40 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 10:30:08 by ishakuro          #+#    #+#             */
-/*   Updated: 2022/03/11 13:29:21 by ishakuro         ###   ########.fr       */
+/*   Updated: 2022/03/14 11:32:10 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-#include "fdf_sourses/minilibx/mlx.h"
-#include <math.h>
-#include <errno.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include "libft.h"
+# include "fdf_sourses/minilibx/mlx.h"
+# include <math.h>
+# include <errno.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include "libft.h"
+
+typedef struct s_coords
+{
+	int				z;
+	struct s_coords	*next;
+}				t_coords;
+
+typedef struct s_map
+{
+	int		width;
+	int		heihgt;
+	int		**lines;
+	int		lines_capacity; //Size of memory allocated for number of lines (int pointers to lines)
+}				t_map;
+
+typedef struct s_point
+{
+	int		x;
+	int		y;
+	int		z;
+	int		color;
+}				t_point;
 
 #endif
--- main.c
+++ main.c
@@ -6,19 +6,102 @@
 /*   By: ishakuro <ishakuro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/11 10:30:08 by ishakuro          #+#    #+#             */
-/*   Updated: 2022/03/11 14:53:24 by ishakuro         ###   ########.fr       */
+/*   Updated: 2022/03/14 11:46:14 by ishakuro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	main(int argc, char **argv)
+t_map	*init_map(void)
 {
-	int	fd;
+	t_map	*map;
+	
+	map = malloc(sizeof(t_map));
+	if (!map)
+		return (NULL);
+	map->width = 0;
+	map->heihgt = 0;
+	map->lines_capacity = 16;
+	map->lines = malloc(sizeof(int) * map->lines_capacity);
+	if (!map->lines)
+	{
+		free(map);
+		return (NULL);
+	}
+	return (map);
+}
+
+int		fill_struct(char **splitted_line, int width, t_map *map)
+{
+	int	*int_line;
+
+	if (!map->width)
+		map->width = width;
+	else if (width != map->width)
+		return (0);
+	int_line = malloc(sizeof(int) * (width));
+	if (!int_line)
+		return (0);
+	
+}
+
+int		read_map(const int fd, t_map *map)
+{
+	int		read_ret;
+	char	*line;
+	char	**splitted_line;
+	int		width;
+	
+	width = 0;
+	read_ret = 1;
+	while (read_ret == 1)
+	{
+		read_ret = get_next_line(fd, &line);
+		splitted_line = ft_strsplit(line, ' ');
+		if (!splitted_line)
+		{
+			ft_strdel(&line)
+			return (0);
+		}
+		while (splitted_line[width] != NULL)
+			width++;
+		if (!fill_struct(splitted_line, width, map))
+		{
+			ft_arraydel(splitted_line, width);
+			ft_strdel(&line)
+			return (0);
+		}
+		
+	}
+	return (read_ret);
+}
+
+int		main(int argc, char **argv)
+{
+	const int	fd;
+	char		*line;
+	t_map		*map;
+
 	if (argc != 2)
 	{
-		ft_putendl_fd("usage: ./fdf <map_file>", 2);
-		return (1);
+		ft_putendl_fd("Usage: ./fdf <map_file>", 2);
+		exit (1);
 	}
 	fd = open(argv[1], O_RDONLY);
+	if (fd < 0)
+	{
+		ft_putendl_fd("Failed to open a map.", 2);
+		exit (1);
+	}
+	map = init_map();
+	if (!map)
+	{
+		ft_putendl_fd("Failed to initialize map.", 2);
+		exit (1);
+	}
+	if (!read_map(fd, map))
+	{
+		ft_putendl_fd("Failed to read a map.", 2);
+		exit (1);
+	}
 }
